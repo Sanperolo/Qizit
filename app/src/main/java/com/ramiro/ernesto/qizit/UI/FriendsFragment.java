015d19102abda12d28da1baf966771fe76136dd6
Ernesto Ramiro <atomms@gmail.com>
2015-01-30 17:15:29 +0100

    fragments with empty messages again

--- app/src/main/java/com/ramiro/ernesto/qizit/UI/FriendsFragment.java
+++ app/src/main/java/com/ramiro/ernesto/qizit/UI/FriendsFragment.java
@@ -2,22 +2,27 @@ package com.ramiro.ernesto.qizit.UI;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
 
 import com.ramiro.ernesto.qizit.R;
 
 /**
  * Created by ernesto on 27/01/15.
  */
-public class FriendsFragment extends ListFragment {
+public class FriendsFragment extends Fragment {
+    protected GridView mGridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        mGridView = (GridView)rootView.findViewById(R.id.friendsGrid);
         return rootView;
     }
 }
