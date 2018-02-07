41a1864b484f206e24fab8da6d46103dcae2f0e2
Ernesto Ramiro <atomms@gmail.com>
2018-02-07 12:44:03 +0100

    now with a swiperefresh layout

--- app/src/main/java/com/ramiro/ernesto/qizit/MainContextActivity.java
+++ app/src/main/java/com/ramiro/ernesto/qizit/MainContextActivity.java
@@ -1,8 +1,10 @@
 package com.ramiro.ernesto.qizit;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -16,6 +18,7 @@ import android.widget.Toast;
 
 public class MainContextActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,29 @@ public class MainContextActivity extends AppCompatActivity {
 //        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
 
+
+//        cast a la vista a la que aplicamos un menu contextual y la registramos
         TextView mycontext = (TextView) findViewById(R.id.textView);
-    registerForContextMenu(mycontext);
+        registerForContextMenu(mycontext);
+
+
+//       cast al Layout SwipeRefresh con el que rodeamos la vista en el xml y le colocamos un listener
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
 
     }
 
+//    construimos el Listener que lanza un Toast y desactiva a continuación del Swipe la animación
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast0 = Toast.makeText(MainContextActivity.this, "going swipeREFRESH", Toast.LENGTH_LONG);
+            toast0.show();
+            swipeLayout.setRefreshing(false);
+        }
+    };
+
+//    inflamos el menu del AppBar con los items del recurso xml
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -39,6 +60,7 @@ public class MainContextActivity extends AppCompatActivity {
         return true;
     }
 
+//    creamos una lista de eventos para los items del menu del AppBar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -51,13 +73,14 @@ public class MainContextActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.camera) {
-            Toast toast = Toast.makeText(this,"going APPBAR CAMERA", Toast.LENGTH_LONG );
+            Toast toast = Toast.makeText(this,"going APPBaR CAMERA", Toast.LENGTH_LONG );
             toast.show();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+//    inflamos el menu contextual con los items del menu xml correspondiente
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
@@ -68,6 +91,7 @@ public class MainContextActivity extends AppCompatActivity {
 
 }
 
+//    creamos una lista de eventos para los items del menus contextual
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
